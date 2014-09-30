97bb55eb52aed2965f1e8d945d9f9e58ccb297f1
TRSturbo <AaronT@Aarons-CrackBook-2.local>
2014-09-30 17:24:43 -0400

    Initial Commit

---
+++ Spiggy/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Spiggy
+//
+//  Created by Aaron Tharpe on 9/30/14.
+//  Copyright (c) 2014 Corpe Incorporated. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

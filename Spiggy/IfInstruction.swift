02669fd197fda655519d1ff56d3ba5ffcd6306da
Kevin Cooper <k.coopr@gmail.com>
2014-09-30 19:59:46 -0700

    minor changes

---
+++ Spiggy/IfInstruction.swift
@@ -0,0 +1,15 @@
+//
+//  IfInstruction.swift
+//  Spiggy
+//
+//  Created by Aaron Tharpe and Kevin Cooper on 9/30/14.
+//  Copyright (c) 2014 Corpe Incorporated. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class ifInstruction: Instruction {
+
+
+}
